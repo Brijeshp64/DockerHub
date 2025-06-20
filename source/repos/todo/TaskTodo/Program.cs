15ccc1e9c2e39df37dd2f8b991a732e946509e1e
Brijeshp64 <brijeshpatel25304@gmail.com>
2025-06-20 16:05:48 +0530

    Restored project with Dockerfile

---
+++ source/repos/todo/TaskTodo/Program.cs
@@ -0,0 +1,117 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using TaskTodo.DAL.AccessService;
+using TaskTodo.DAL.Repository;
+using TaskTodo.Data;
+using TaskTodo.Services.intreface;
+using TaskTodo.Services.Repo;
+
+var builder = WebApplication.CreateBuilder(args);
+var Myallowedports = "AllowLocalhost3000";
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbSa = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+var connectionString = $"Server={dbHost};Database={dbName};User Id=sa;Password={dbSa};TrustServerCertificate=True;";
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ITask, TaskService>();
+builder.Services.AddScoped<ITaskAuth, TaskAuth>();
+builder.Services.AddScoped<IAuthenticate, Authenticate>();
+builder.Services.AddScoped<IUser, UserService>();
+ builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new() { Title = "MyAPI", Version = "v1" });
+
+    // Add JWT Bearer Auth to Swagger
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Enter 'Bearer' [space] and then your valid token.\n\nExample: Bearer abc123xyz"
+    });
+
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+builder.Services.AddAuthorization();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: Myallowedports,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://localhost:61634", "https://localhost:61635")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                      });
+});
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(8080); // MUST match docker internal port
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(Myallowedports);
+app.UseAuthentication(); 
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
---
+++ source/repos/todo/TaskTodo/Services/intreface/ITask.cs
@@ -0,0 +1,21 @@
+﻿using TaskTodo.Model.DTO;
+using TaskTodo.Model.Entity;
+
+namespace TaskTodo.Services.intreface
+{
+    public interface ITask
+    {
+        public Task<(int StatusCode,IEnumerable<TaskData> data ,bool Result)> GetAllTask();
+        public Task<(int StatusCode, bool Result)> AddTask(AddTaskRequest req);
+        public Task<(int StatusCode,List<TaskData> data ,bool Result)> GetTaskById(int id);
+        public Task<(int StatusCode, bool Result)> TaskUpdate(int id,AddTaskRequest req);
+        public Task<(int StatusCode, bool Result)> TaskDelete(int id);
+        public Task<(int StatusCode, bool Result)> TaskRestore(int id);
+        public Task<(int StatusCode, bool Result)> TaskComplete(int id);
+        public Task<(int StatusCode, IEnumerable<TaskData> data, bool Result)> GetAllDeletedTask();
+        public Task<(int StatusCode,bool Result)> DeleteParmenetly(int id);
+        public Task<(int StatusCode, bool Result)> massUpload(IFormFile file);
+        public Task<(int StatusCode, IEnumerable<TaskData> data, bool Result)> GetExcel();
+
+    }
+}
